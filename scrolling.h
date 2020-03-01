e0ee97a43bd618bcb91aa5ff2954f1db6b9bb129
ahmedmanaa210 <61382675+ahmedmanaa210@users.noreply.github.com>
2020-03-01 14:38:46 +0100

    Add files via upload

---
+++ scrolling.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_mixer.h>
+#include <string.h>
+
+
+#ifndef DEF_CONSTANTES
+#define DEF_CONSTANTES
+
+
+
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define SHEET_WIDTH 2400 
+#define SHEET_HEIGHT 600
+#define FPS 30
+#define speed 5
+
+SDL_Rect scroll_function(SDL_Rect *camera , int *b , SDL_Surface *ecran , SDL_Surface *background);
+
+
+#endif 
